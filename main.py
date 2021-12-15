84a66e31e1a0992f1f31feaa21edea3405e8df1c
Evgenion47 <rederror12Q>
2021-12-15 10:15:57 +0500

    aaaaaaaaaaaa

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from math import sqrt
 from sympy import *
 
 x, y = symbols('x,y')
-f = pow(x, 2) + pow((y - 3), 2)
+f = 4 * pow(x, 2) + 3 * pow(y, 2) - 4 * x * y + x
 
 def getAgrad(x1, x2):
     return [agrad(diff(f, x), x1, x2), agrad(diff(f, y), x1, x2)]
@@ -12,21 +12,20 @@ def getAgrad(x1, x2):
 def agrad(x, x1, x2):
     return -1 * eval(str(x).replace(" ", "").replace("x", str(x1)).replace("y", str(x2)))
 
-def processing(v, eps, x1, x2):  # v - выбор методы : 1 метод наискорейшего спуска, 2 метод сопреженных гардиентов
+def processing(v, eps, x1, x2):  # v - выбор метода : 1 метод наискорейшего спуска, 2 метод сопреженных гардиентов
     k, b1, b2 = 0, 0, 0
-    agradx1, agradx2 = getAgrad(x1, x2)
-    table = [['N', 'x(n)', 'S(n)'], [k, f'({x1};{x2})', f'({agradx1};{agradx2})']]
-    while sqrt(agradx1 ** 2 + agradx2 ** 2) >= eps:
-        min, k = golden(lambda z: lambdify((x, y), f)(x1 + agradx1 * z, x2 + agradx2 * z)), k + 1
-        x1, x2 = x1 + agradx1 * min, x2 + agradx2 * min
+    agradx = getAgrad(x1, x2)
+    table = [['N', 'x(n)', 'S(n)'], [k, f'({x1};{x2})', f'({agradx[0]};{agradx[1]})']]
+    while sqrt(pow(agradx[0], 2) + pow(agradx[1], 2)) >= eps:
+        min, k = golden(lambda z: lambdify((x, y), f)(x1 + agradx[0] * z, x2 + agradx[1] * z)), k + 1
+        x1, x2 = x1 + agradx[0] * min, x2 + agradx[1] * min
         if v == 1:
-            agradx1, agradx2 = getAgrad(x1, x2)
+            agradx = getAgrad(x1, x2)
         else:
-            b1, b2 = pow(getAgrad(x1, x2)[0], 2) / pow(agradx1, 2), pow(getAgrad(x1, x2)[1], 2) / pow(agradx2, 2)
-            agradx1, agradx2 = getAgrad(x1, x2)[0] + b1 * agradx1, getAgrad(x1, x2)[1] + b2 * agradx2
-        table.append([k, f'({x1};{x2})', f'({agradx1};{agradx2})'])
+            tmpAgrad = getAgrad(x1,x2)
+            b = pow(pow(tmpAgrad[0],2) + pow(tmpAgrad[1],2), 2) / pow(pow(agradx[0],2) + pow(agradx[1],2), 2)
+            agradx[0], agradx[1] = getAgrad(x1, x2)[0] + b * agradx[0], getAgrad(x1, x2)[1] + b * agradx[1]
+        table.append([k, f'({x1};{x2})', f'({agradx[0]};{agradx[1]})'])
+    print(tabulate(table, headers='keys') + '\n')
 
-    print(sqrt(pow(agradx1, 2) + pow(agradx2, 2)))
-    print(tabulate(table, headers='keys')+'\n')
-
-processing(1, 0.0000001, 20, -34), processing(2, 0.0000001, 20, -34)
+processing(1, 0.0000001, 0, 0), processing(2, 0.0001, 0, 0)
