50a867f56f3d1d5641af652e71cc67c9d91366f2
Evgenion47 <rederror12Q>
2021-12-15 10:22:25 +0500

    aaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ def agrad(x, x1, x2):
     return -1 * eval(str(x).replace(" ", "").replace("x", str(x1)).replace("y", str(x2)))
 
 def processing(v, eps, x1, x2):  # v - выбор метода : 1 метод наискорейшего спуска, 2 метод сопреженных гардиентов
-    k, b1, b2 = 0, 0, 0
-    agradx = getAgrad(x1, x2)
+    agradx, k = getAgrad(x1, x2), 0
     table = [['N', 'x(n)', 'S(n)'], [k, f'({x1};{x2})', f'({agradx[0]};{agradx[1]})']]
     while sqrt(pow(agradx[0], 2) + pow(agradx[1], 2)) >= eps:
         min, k = golden(lambda z: lambdify((x, y), f)(x1 + agradx[0] * z, x2 + agradx[1] * z)), k + 1
@@ -22,7 +21,7 @@ def processing(v, eps, x1, x2):  # v - выбор метода : 1 метод н
         if v == 1:
             agradx = getAgrad(x1, x2)
         else:
-            b = pow(pow(getAgrad(x1,x2)[0],2) + pow(getAgrad(x1,x2)[1],2), 2) / pow(pow(agradx[0],2) + pow(agradx[1],2), 2)
+            b = pow(pow(getAgrad(x1, x2)[0], 2) + pow(getAgrad(x1, x2)[1], 2), 2) / pow(pow(agradx[0], 2) + pow(agradx[1], 2), 2)
             agradx[0], agradx[1] = getAgrad(x1, x2)[0] + b * agradx[0], getAgrad(x1, x2)[1] + b * agradx[1]
         table.append([k, f'({x1};{x2})', f'({agradx[0]};{agradx[1]})'])
     print(tabulate(table, headers='keys') + '\n')
